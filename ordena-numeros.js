288ca4fadfbd46acc35066a87f8278c476fc8322
natsm <natsmarinho@gmail.com>
2023-05-17 18:07:04 -0300

    docs: Comentarios de como funciona a comparacao adicionada

--- ordena-numeros.js
+++ ordena-numeros.js
@@ -1,8 +1,10 @@
-const listaNum = [2, 5, 6, 9, 12];
+const listaNum = [2, 6, 5,  9, 12, 33, 32];
 
 function orderna(numeros) {
    return numeros.sort(function (a, b) {
-    return a - b
+    return a - b 
+    //comparamos b com a, se o resultado for negativo o a vem primeiro e se for positivo o b vem primeiro, se for zero a ordem é a mesma
+    //a comparação é feita para cada par dentro da array
    })
 }
 
