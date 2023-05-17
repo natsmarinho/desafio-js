57dd4f9b781d6dbdae0a72bc25dbb927cbeed1b2
natsm <natsmarinho@gmail.com>
2023-05-17 18:09:13 -0300

    docs: Funcionalidade de comparacao feita dentro do sort

--- ordena-numeros.js
+++ ordena-numeros.js
@@ -4,7 +4,8 @@ function orderna(numeros) {
    return numeros.sort(function (a, b) {
     return a - b 
     //comparamos b com a, se o resultado for negativo o a vem primeiro e se for positivo o b vem primeiro, se for zero a ordem é a mesma
-    //a comparação é feita para cada par dentro da array
+    //a comparação é feita para cada par dentro da array 
+    
    })
 }
 
