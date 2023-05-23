885bbf4882334efd1c039a30d1786daa4f230d1e
natsm <natsmarinho@gmail.com>
2023-05-23 17:53:14 -0300

    feat: Funcao que calcula a media de tres numeros

---
+++ media.js
@@ -0,0 +1,6 @@
+function calculaMedia(num1, num2, num3) {
+    const somaAritimetica = num1 + num2 + num3;
+    return ` A média é: ${somaAritimetica / 3}` 
+}
+
+console.log(calculaMedia(12, 25, 88))
