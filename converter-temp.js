c359399182ab5c43db986fc01f29e3b21320f076
natsm <natsmarinho@gmail.com>
2023-05-29 18:00:48 -0300

    feat: Funcao que converte de celcius para Fahrenheit

---
+++ converter-temp.js
@@ -0,0 +1,6 @@
+function converteTemperatura(celcius) {
+    const fahrenheit = (celcius * 1.8) + 32;
+    return `A temperatura em ${celcius} é de: ${fahrenheit}`;
+}
+
+console.log(converteTemperatura(32));
