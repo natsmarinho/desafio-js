b4c3a154553750445461537cae6d5038c454af95
natsm <natsmarinho@gmail.com>
2023-06-28 17:57:51 -0300

    feat: Funcao que converte km em anos luz

---
+++ anos-luz.js
@@ -0,0 +1,8 @@
+const anoLuzConvertido = 9460528405000;
+
+function converteAnoLuz(anoLuz) {
+    const calculoAnosLuz = anoLuzConvertido * anoLuz;
+    return `Cerca de ${calculoAnosLuz} anos luz`
+}
+
+console.log(converteAnoLuz(4))
